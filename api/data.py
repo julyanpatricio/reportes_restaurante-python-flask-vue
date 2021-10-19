e2dc15594a056a3261d83e7e6f1acbedf72776e9
julyanpatricio <julyan.patricio@gmail.com>
2021-10-19 17:01:44 -0300

    erase products in dict

--- api/data.py
+++ api/data.py
@@ -5,6 +5,7 @@ sales = json.loads(resp.content)
 
 
 salesGroupByCategories={}
+salesGroupByCategoriesAndMonths={}
 salesGroupByProducts={}
 for sale in sales:
   for product in sale['products']:
@@ -12,20 +13,16 @@ for sale in sales:
       salesGroupByCategories[product['category'].lower()] = {
         'total quantities':product['quantity'],
         'total incomes':product['quantity'] * product['price'],
-        'products':[product]
         }
     else:
       salesGroupByCategories[product['category'].lower()]['total quantities'] += product['quantity']
       salesGroupByCategories[product['category'].lower()]['total incomes'] += product['quantity'] * product['price']
-      salesGroupByCategories[product['category'].lower()]['products'].append(product)
     
     if not salesGroupByProducts.get(product['name'].lower()):
       salesGroupByProducts[product['name'].lower()] = {
         'total quantities':product['quantity'],
         'total incomes':product['quantity'] * product['price'],
-        'products':[product]
         }
     else:
       salesGroupByProducts[product['name'].lower()]['total quantities'] += product['quantity']
       salesGroupByProducts[product['name'].lower()]['total incomes'] += product['quantity'] * product['price']
-      salesGroupByProducts[product['name'].lower()]['products'].append(product)
