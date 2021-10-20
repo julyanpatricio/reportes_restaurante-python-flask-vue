82ed38525a1b6d56c25eea43c638276e4242e6b8
julyanpatricio <julyan.patricio@gmail.com>
2021-10-19 23:05:35 -0300

    style

--- client/src/App.vue
+++ client/src/App.vue
@@ -15,6 +15,9 @@ export default {
 </script>
 
 <style>
+html, body, #app{
+  height: -webkit-fill-available;
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
