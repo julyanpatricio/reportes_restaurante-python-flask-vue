3ce4ab2494ee0ab409d9d11c77a692bc52db51d5
julyanpatricio <julyan.patricio@gmail.com>
2021-10-19 23:02:31 -0300

    select-option

--- client/src/components/AppHeader.vue
+++ client/src/components/AppHeader.vue
@@ -2,17 +2,35 @@
   <header>
     <span class="title">La pikada de la esquina</span>
     <router-link to="/" class="btn-header"> home </router-link>
+    <select name="rutas" id="rutas" v-on:click="redirectTo">
+      <option value="/">home</option>
+      <option value="/ventas">ventas generales</option>
+      <option value="/ventas/categorias">ventas por categorias</option>
+    </select>
   </header>
 </template>
 
 <script>
+import router from '@/router'
 export default {
-  name: 'app-header'
+  name: 'app-header',
+  methods: {
+    redirectTo: (e) => router.push(e.target.value)
+  }
 }
 </script>
 
 <style scoped>
-header{
+header {
   display: block;
+  height: 8%;
+  background-color: brown;
+  color: aliceblue;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  font-size: x-large;
+  padding-inline: 2%;
 }
 </style>
